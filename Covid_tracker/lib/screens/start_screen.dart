6b234fdf58c12821e36d61ce001bfd42831813e6
manish <manishkumar.194ca024@nitk.edu.in>
2020-10-21 08:44:51 +0530

    Basic of home page -menu

---
+++ Covid_tracker/lib/screens/start_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class StartScreen extends StatefulWidget {
+  @override
+  _StartScreenState createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
